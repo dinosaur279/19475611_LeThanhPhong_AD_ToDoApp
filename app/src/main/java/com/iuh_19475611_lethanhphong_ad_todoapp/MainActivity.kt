711fb74a3bbb8c10efcd2d0a5e7fdf642fd498e3
phong2709 <thanhphong27092001@gmail.com>
2021-12-10 16:09:55 +0700

    ok

---
+++ app/src/main/java/com/iuh_19475611_lethanhphong_ad_todoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.iuh_19475611_lethanhphong_ad_todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
